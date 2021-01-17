5749b77e803b40ae980698163a4e3288ebaaa766
Haruncan Atalı <77530565+haruncanatali@users.noreply.github.com>
2021-01-17 15:48:29 +0300

    yilanOyunu

---
+++ YilanOyunu/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace YilanOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gecenSureLbl = new System.Windows.Forms.Label();
+            this.puanLbl = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gecenSureTimer = new System.Windows.Forms.Timer(this.components);
+            this.yilanTimer = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.intervalLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(68, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Puan :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(13, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Geçen Süre :";
+            // 
+            // gecenSureLbl
+            // 
+            this.gecenSureLbl.AutoSize = true;
+            this.gecenSureLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gecenSureLbl.ForeColor = System.Drawing.Color.Red;
+            this.gecenSureLbl.Location = new System.Drawing.Point(134, 55);
+            this.gecenSureLbl.Name = "gecenSureLbl";
+            this.gecenSureLbl.Size = new System.Drawing.Size(0, 15);
+            this.gecenSureLbl.TabIndex = 2;
+            // 
+            // puanLbl
+            // 
+            this.puanLbl.AutoSize = true;
+            this.puanLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.puanLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.puanLbl.Location = new System.Drawing.Point(134, 25);
+            this.puanLbl.Name = "puanLbl";
+            this.puanLbl.Size = new System.Drawing.Size(0, 15);
+            this.puanLbl.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(131, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "YILAN OYUNU";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Black;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(25, 63);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(400, 400);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.intervalLabel);
+            this.groupBox1.Controls.Add(this.gecenSureLbl);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.puanLbl);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Location = new System.Drawing.Point(446, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 159);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Oyun";
+            // 
+            // gecenSureTimer
+            // 
+            this.gecenSureTimer.Enabled = true;
+            this.gecenSureTimer.Interval = 1000;
+            this.gecenSureTimer.Tick += new System.EventHandler(this.gecenSureTimer_Tick);
+            // 
+            // yilanTimer
+            // 
+            this.yilanTimer.Enabled = true;
+            this.yilanTimer.Interval = 150;
+            this.yilanTimer.Tick += new System.EventHandler(this.yilanTimer_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(22, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Kalan Yem :";
+            // 
+            // intervalLabel
+            // 
+            this.intervalLabel.AutoSize = true;
+            this.intervalLabel.Location = new System.Drawing.Point(134, 88);
+            this.intervalLabel.Name = "intervalLabel";
+            this.intervalLabel.Size = new System.Drawing.Size(25, 13);
+            this.intervalLabel.TabIndex = 3;
+            this.intervalLabel.Text = "150";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "150 adet yem ye oyunu kazan!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(658, 475);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label3);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yılan Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label gecenSureLbl;
+        private System.Windows.Forms.Label puanLbl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer gecenSureTimer;
+        private System.Windows.Forms.Timer yilanTimer;
+        private System.Windows.Forms.Label intervalLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ YilanOyunu/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YilanOyunu
+{
+    public partial class Form1 : Form
+    {
+
+        LinkedList<Label> yilan = new LinkedList<Label>();
+        List<string> duvarlar_1 = new List<string>();
+        List<string> duvarlar_2 = new List<string>();
+        int yon = 0, gecenSure = 0;
+        bool sag = false, sol = false;
+        public double puan = 0.0;
+        string istikamet = "sag", yem = null, sonParca = null, deger = null;
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Baslangic();
+            DuvarTanimla();
+            sagYon();
+        }
+
+        private void DuvarTanimla()
+        {
+            for (int i = 0; i <= 380; i+=20)
+            {    
+                duvarlar_1.Add(i.ToString());
+            }
+            for (int i = 19; i <= 399; i += 20)
+            {
+                duvarlar_2.Add(i.ToString());
+            }
+        }
+
+        private void Yem()
+        {
+        x:
+            yem = random.Next(399).ToString();
+            foreach (Label item in flowLayoutPanel1.Controls)
+            {
+                if (item.Name == yem && item.BackColor == Color.Green)
+                {
+                    goto x;
+                }
+            }
+            foreach (Label item in flowLayoutPanel1.Controls)
+            {
+                if (item.Name == yem)
+                {
+                    item.BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void Baslangic()
+        {
+            gecenSureTimer.Interval = 1000;
+
+            for (int i = 0; i < 400; i++)
+            {
+                Label label = new Label();
+                label.Height = 20;
+                label.Width = 20;
+                label.Name = i.ToString();
+                //label.Text = i.ToString();
+                label.BackColor = Color.White;
+                label.Margin = new Padding(0, -1, -1, -1);
+
+                flowLayoutPanel1.Controls.Add(label);
+            }
+
+            foreach (Label item in flowLayoutPanel1.Controls)
+            {
+                if (item.Name == "229")
+                {
+                    yilan.AddLast(item);
+                    item.BackColor = Color.Green;
+                }
+                else if (item.Name == "230" || item.Name == "231")
+                {
+                    yilan.AddFirst(item);
+                    item.BackColor = Color.Green;
+                }
+                if (item.Name == "229")
+                {
+                    sonParca = item.Name;
+                }
+            }
+            Yem();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.Up:
+                    if (istikamet != "asagi") {
+                        yukariYon();
+                    }
+                    break;
+                case Keys.Down:
+                    if (istikamet != "yukari") 
+                    {
+                        asagiYon();
+                    }
+                    break;
+                case Keys.Left:
+                    if (istikamet != "sag")
+                    {
+                        solYon();
+                    }
+                    break;
+                case Keys.Right:
+                    if(istikamet != "sol"){
+                        sagYon();
+                    }
+                    break;
+            }
+        }
+
+
+        private void sagYon() {
+            yon = 1;
+            istikamet = "sag";
+            YonVer();
+        }
+        private void solYon() {
+            yon = -1;
+            istikamet = "sol";
+            YonVer();
+        }
+        private void yukariYon() {
+            yon = -20;
+            istikamet = "yukari";
+            YonVer();
+        }
+        private void asagiYon()
+        {
+            yon = 20;
+            istikamet = "asagi";
+            YonVer();
+        }
+
+        private void yilanTimer_Tick(object sender, EventArgs e)
+        {
+            YonVer();
+        }
+
+       
+        private void YonVer()
+        {
+            bool durum = false;
+            LinkedListNode<Label> dugum = yilan.First;
+
+            deger = (Convert.ToInt32(dugum.Value.Name) + (yon)).ToString();
+
+            if (DuvarKontrol(int.Parse(deger)))
+            {
+                foreach (Label item in flowLayoutPanel1.Controls)
+                {
+                    if (item.Name == deger)
+                    {
+
+                        yilan.AddFirst(item);
+                        if (item.BackColor == Color.Red)
+                        {
+                            puan += 50;
+                            puanLbl.Text = puan.ToString();
+                            foreach (Label item_ in flowLayoutPanel1.Controls)
+                            {
+                                if (item_.Name == sonParca)
+                                {
+                                    yilan.AddLast(item_);
+                                    item_.BackColor = Color.Green;
+                                    durum = true;
+                                    yilanTimer.Interval -= 1;
+                                    intervalLabel.Text = yilanTimer.Interval.ToString();
+
+                                    if(yilanTimer.Interval == 0)
+                                    {
+                                        OyunBitti();
+                                    }
+
+                                    Yem();
+                                }
+                            }
+                        }
+                        item.BackColor = Color.Green;
+
+                    }
+                }
+
+                if (durum == false)
+                {
+                    dugum = yilan.Last;
+
+                    foreach (Label item in flowLayoutPanel1.Controls)
+                    {
+                        if (item.Name == dugum.Value.Name.ToString())
+                        {
+                            item.BackColor = Color.White;
+                            sonParca = item.Name;
+                            yilan.RemoveLast();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                OyunBitti();
+            }
+
+        }
+
+
+        private void OyunBitti()
+        {
+            yilanTimer.Enabled = false;
+            gecenSureTimer.Enabled = false;
+            DialogResult result = MessageBox.Show("Oyun Bitti Puanınız :" + puanLbl.Text.ToString() + " Yeniden Başlamak istiyormusunuz ?", "Bilgi", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Restart();
+            }
+            else if (result == DialogResult.No)
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private void gecenSureTimer_Tick(object sender, EventArgs e)
+        {
+            gecenSure++;
+            gecenSureLbl.Text = gecenSure.ToString()+" s";
+        }
+        
+        private bool DuvarKontrol(int konum)
+        {
+            bool durum=true;
+            foreach (Label item in flowLayoutPanel1.Controls)
+            {
+                if(item.Name == konum.ToString())
+                {
+                    if(item.BackColor == Color.Green)
+                    {
+                        durum = false;
+                    }
+                }
+            }
+            if(konum<0 || konum >399)
+            {
+                durum = false;
+            }
+
+            
+
+
+            if (istikamet == "sol" && sol)
+            {
+                durum = false;
+            }
+            else if (istikamet == "sag" && sag)
+            {
+                durum = false;
+            }
+
+
+            if (istikamet == "sol")
+            {
+                sag = false;
+                if (duvarlar_1.Contains((Convert.ToInt32(konum)).ToString()))
+                {
+                    sol = true;
+                    
+                }
+            }
+            
+            else if(istikamet == "sag")
+            {
+                sol = false;
+                if (duvarlar_2.Contains((Convert.ToInt32(konum)).ToString()))
+                {
+                    sag = true;
+                    
+                }
+            }
+            
+            return durum;
+        }
+
+    }
+}
